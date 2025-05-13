7e3a3948341f212c6ba5c42fb9a133938508090a
Abhyaung Deepak Havale <abhyaung@gmail.com>
2025-05-12 21:43:14 -0400

    Delete rideease-backend directory

--- rideease-backend/src/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import authController from '../controllers/authController.js'
-const router = express.Router();
-
-router.post('/signup',authController.signupHandler);
-router.post('/login',authController.loginHandler);
-router.post('/forgot-password', authController.resetPasswordHandler)
-
-export default router;
